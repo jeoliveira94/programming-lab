56708b832a9d0db617cedf3a048d0b4de39325c6
José Oliveira <eduardo.jeso@gmail.com>
2023-06-27 11:40:08 -0300

    Spring Web skeleton added

---
+++ Java/Spring/SpringLab/Web/WebApps/src/main/java/com/ruemmai/WebApps/WebAppsApplication.java
@@ -0,0 +1,13 @@
+package com.ruemmai.WebApps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebAppsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebAppsApplication.class, args);
+	}
+
+}
