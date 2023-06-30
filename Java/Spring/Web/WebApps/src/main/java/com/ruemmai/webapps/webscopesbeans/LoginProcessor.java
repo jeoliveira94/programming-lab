9e55edcc41ab08e4433c3211d93fa7f71787837f
José Oliveira <eduardo.jeso@gmail.com>
2023-06-30 17:51:05 -0300

    [WebScope] Using Request Scope Bean to authentication

---
+++ Java/Spring/Web/WebApps/src/main/java/com/ruemmai/webapps/webscopesbeans/LoginProcessor.java
@@ -0,0 +1,34 @@
+package com.ruemmai.webapps.webscopesbeans;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.context.annotation.RequestScope;
+
+@Component
+@RequestScope
+public class LoginProcessor {
+    private String username;
+    private String password;
+
+    public boolean login() {
+        String username = this.getUsername();
+        String password = this.getPassword();
+
+        return "joliveira".equals(username) && "1234".equals(password);
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
