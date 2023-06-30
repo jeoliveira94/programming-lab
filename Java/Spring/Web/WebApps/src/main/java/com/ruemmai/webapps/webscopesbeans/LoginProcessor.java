ef993428b6b7d5a731cf16186987bbc111d0bf04
José Oliveira <eduardo.jeso@gmail.com>
2023-06-30 18:11:07 -0300

    [WebScope] Using Session Scope Bean to make app aware that user is logged

--- Java/Spring/Web/WebApps/src/main/java/com/ruemmai/webapps/webscopesbeans/LoginProcessor.java
+++ Java/Spring/Web/WebApps/src/main/java/com/ruemmai/webapps/webscopesbeans/LoginProcessor.java
@@ -6,14 +6,25 @@ import org.springframework.web.context.annotation.RequestScope;
 @Component
 @RequestScope
 public class LoginProcessor {
+    private final LoggedUserManagementService loggedUserManagementService;
     private String username;
     private String password;
+
+    public LoginProcessor(
+            LoggedUserManagementService loggedUserManagementService) {
+        this.loggedUserManagementService = loggedUserManagementService;
+    }
 
     public boolean login() {
         String username = this.getUsername();
         String password = this.getPassword();
 
-        return "joliveira".equals(username) && "1234".equals(password);
+        boolean loginResult = false;
+        if ("joliveira".equals(username) && "1234".equals(password)) {
+            loginResult = true;
+            loggedUserManagementService.setUsername(username);
+        }
+        return loginResult;
     }
 
     public String getUsername() {
