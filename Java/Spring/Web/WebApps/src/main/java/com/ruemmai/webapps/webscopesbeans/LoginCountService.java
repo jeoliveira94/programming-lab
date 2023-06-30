e02c1265626264812e6d19784c2210b847d6d689
José Oliveira <eduardo.jeso@gmail.com>
2023-06-30 18:23:06 -0300

    [WebScope] Using Application Scope Bean to count logins

---
+++ Java/Spring/Web/WebApps/src/main/java/com/ruemmai/webapps/webscopesbeans/LoginCountService.java
@@ -0,0 +1,16 @@
+package com.ruemmai.webapps.webscopesbeans;
+
+import org.springframework.stereotype.Service;
+import org.springframework.web.context.annotation.ApplicationScope;
+
+@Service
+@ApplicationScope
+public class LoginCountService {
+    private int count;
+    public void increment() {
+        count++;
+    }
+    public int getCount() {
+        return count;
+    }
+}
--- Java/Spring/Web/WebApps/src/main/java/com/ruemmai/webapps/webscopesbeans/MainController.java
+++ Java/Spring/Web/WebApps/src/main/java/com/ruemmai/webapps/webscopesbeans/MainController.java
@@ -8,9 +8,12 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class MainController {
     private final LoggedUserManagementService loggedUserManagementService;
+    private final LoginCountService loginCountService;
+
     public MainController(
-            LoggedUserManagementService loggedUserManagementService) {
+            LoggedUserManagementService loggedUserManagementService, LoginCountService loginCountService) {
         this.loggedUserManagementService = loggedUserManagementService;
+        this.loginCountService = loginCountService;
     }
 
     @GetMapping("/main")
@@ -23,11 +26,15 @@ public class MainController {
         }
 
         String username = loggedUserManagementService.getUsername();
+        int count = loginCountService.getCount();
+
         if (username == null) {
             return "redirect:/login";
         }
 
         model.addAttribute("username" , username);
+        model.addAttribute("loginCount", count);
+
         return "main.html";
     }
 }
