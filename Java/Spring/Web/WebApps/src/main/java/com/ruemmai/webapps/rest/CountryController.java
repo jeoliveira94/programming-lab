28f633c5fbfe1dd3b1140ebeace2a504a417726c
José Oliveira <eduardo.jeso@gmail.com>
2023-07-03 18:27:29 -0300

    [REST] Adding custom headers and setting a response status

--- Java/Spring/Web/WebApps/src/main/java/com/ruemmai/webapps/rest/CountryController.java
+++ Java/Spring/Web/WebApps/src/main/java/com/ruemmai/webapps/rest/CountryController.java
@@ -1,5 +1,7 @@
 package com.ruemmai.webapps.rest;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,9 +10,14 @@ import java.util.List;
 @RestController
 public class CountryController {
     @GetMapping("/france")
-    public Country france() {
+    public ResponseEntity<Country> france() {
         Country country = Country.of("France", 67);
-        return country;
+        return ResponseEntity
+                .status(HttpStatus.ACCEPTED)
+                .header("X-Continent", "Europe")
+                .header("X-Capital", "Paris")
+                .header("X-Favorite_food", "cheese and wine")
+                .body(country);
     }
 
     @GetMapping("/all")
