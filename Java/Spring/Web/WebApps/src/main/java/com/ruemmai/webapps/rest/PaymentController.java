dd62ad45192ba19f533f2287d96ba846e65c12de
José Oliveira <eduardo.jeso@gmail.com>
2023-07-03 18:39:50 -0300

    [REST] Refactor managing exceptions to use a Advice

--- Java/Spring/Web/WebApps/src/main/java/com/ruemmai/webapps/rest/PaymentController.java
+++ Java/Spring/Web/WebApps/src/main/java/com/ruemmai/webapps/rest/PaymentController.java
@@ -7,25 +7,15 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class PaymentController {
-
     private final PaymentService paymentService;
     public PaymentController(PaymentService paymentService) {
         this.paymentService = paymentService;
     }
     @PostMapping("/payment")
-    public ResponseEntity<?> makePayment() {
-        try {
-            PaymentDetails paymentDetails =
-                    paymentService.processPayment();
-            return ResponseEntity
-                    .status(HttpStatus.ACCEPTED)
-                    .body(paymentDetails);
-        } catch (NotEnoughMoneyException e) {
-            ErrorDetails errorDetails = new ErrorDetails();
-            errorDetails.setMessage("Not enough money to make the payment.");
-            return ResponseEntity
-                    .badRequest()
-                    .body(errorDetails);
-        }
+    public ResponseEntity<PaymentDetails> makePayment() {
+        PaymentDetails paymentDetails = paymentService.processPayment();
+        return ResponseEntity
+                .status(HttpStatus.ACCEPTED)
+                .body(paymentDetails);
     }
 }
