c3b5983e293aa1f1aa2a1f991ebc073391b6a94f
José Oliveira <eduardo.jeso@gmail.com>
2023-07-12 10:24:27 -0300

    [Tests] Integration test for money transfer

---
+++ Java/Spring/Web/WebApps/src/test/java/com/ruemmai/webapps/TransferServiceSpringIntegrationTests.java
@@ -0,0 +1,45 @@
+package com.ruemmai.webapps;
+
+import com.ruemmai.webapps.transactions.Account;
+import com.ruemmai.webapps.transactions.AccountRepository;
+import com.ruemmai.webapps.transactions.TransferService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+
+import javax.security.auth.login.AccountNotFoundException;
+import java.math.BigDecimal;
+import java.util.Optional;
+
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+@SpringBootTest
+public class TransferServiceSpringIntegrationTests {
+
+    @MockBean
+    private AccountRepository accountRepository;
+
+    @Autowired
+    private TransferService transferService;
+
+    @Test
+    void transferServiceTransferAmountTest() throws AccountNotFoundException {
+        Account sender = new Account();
+        sender.setId(1);
+        sender.setAmount(new BigDecimal(1000));
+
+        Account receiver = new Account();
+        receiver.setId(2);
+        receiver.setAmount(new BigDecimal(1000));
+
+        when(accountRepository.findAccountById(1L)).thenReturn(Optional.of(sender));
+        when(accountRepository.findAccountById(2L)).thenReturn(Optional.of(receiver));
+
+        transferService.transferMoney(1, 2, new BigDecimal(100));
+
+        verify(accountRepository).changeAmount(1, new BigDecimal(900));
+        verify(accountRepository).changeAmount(2, new BigDecimal(1100));
+    }
+}
