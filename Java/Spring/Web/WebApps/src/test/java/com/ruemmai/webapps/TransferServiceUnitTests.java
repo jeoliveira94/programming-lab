03369f85c14ec6007fac8427b861196e00438dbd
José Oliveira <eduardo.jeso@gmail.com>
2023-07-12 09:44:42 -0300

    [Tests] Unit test for money transfer account not found exception

--- Java/Spring/Web/WebApps/src/test/java/com/ruemmai/webapps/TransferServiceUnitTests.java
+++ Java/Spring/Web/WebApps/src/test/java/com/ruemmai/webapps/TransferServiceUnitTests.java
@@ -14,7 +14,11 @@ import javax.security.auth.login.AccountNotFoundException;
 import java.math.BigDecimal;
 import java.util.Optional;
 
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.verify;
 
 @ExtendWith(MockitoExtension.class)
@@ -46,5 +50,21 @@ public class TransferServiceUnitTests {
         verify(accountRepository).changeAmount(1, new BigDecimal(900));
         verify(accountRepository).changeAmount(2, new BigDecimal(1100));
 
+    }
+
+    @Test
+    public void moneyTransferDestinationAccountNotFoundFlow() {
+        Account sender = new Account();
+        sender.setId(1);
+        sender.setAmount(new BigDecimal(1000));
+
+        given(accountRepository.findAccountById(1L)).willReturn(Optional.of(sender));
+        given(accountRepository.findAccountById(2L)).willReturn(Optional.empty());
+
+        assertThrows(AccountNotFoundException.class,
+                () -> transferService.transferMoney(1, 2, new BigDecimal(100))
+        );
+
+        verify(accountRepository, never()).changeAmount(anyLong(), any());
     }
 }
